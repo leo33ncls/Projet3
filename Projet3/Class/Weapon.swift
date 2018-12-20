bcec2e4fb715a29d11ff06377803d4dccf5502f7
Léo Nicolas <leoncls3398@gmail.com>
2018-12-20 18:19:32 +0100

    Ajout de la class Weapon

--- Projet3/Class/Weapon.swift
+++ Projet3/Class/Weapon.swift
@@ -9,9 +9,12 @@
 import Foundation
 
 class Weapon {
+    
+    // Properties
     var name: String
     var damage: Int
     
+    // Initialization
     init(name: String ,damage: Int) {
         self.name = name
         self.damage = damage

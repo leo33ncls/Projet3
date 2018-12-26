37885ef0372ea3871c7421198c027b845ef9a220
Léo Nicolas <leoncls3398@gmail.com>
2018-12-26 17:13:00 +0100

    Ajout de l'arme du Mage

---
+++ Projet3/Class/Weapon/Wand.swift
@@ -0,0 +1,16 @@
+//
+//  Wand.swift
+//  Projet3
+//
+//  Created by Léo NICOLAS on 26/12/2018.
+//  Copyright © 2018 Léo NICOLAS. All rights reserved.
+//
+
+import Foundation
+
+class Wand: Weapon {
+    
+    init() {
+        super.init(name: "baguette magique", damage: -10)
+    }
+}

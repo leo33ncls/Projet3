819d9f1f7471b0fb49742dc1789acaf3d0ef7fd4
Léo Nicolas <leoncls3398@gmail.com>
2018-12-29 22:00:13 +0100

    Bonus: Ajout de l'arc Bow()

---
+++ Projet3/Class/Weapon/Bow.swift
@@ -0,0 +1,23 @@
+//
+//  Bow.swift
+//  Projet3
+//
+//  Created by Léo NICOLAS on 29/12/2018.
+//  Copyright © 2018 Léo NICOLAS. All rights reserved.
+//
+
+import Foundation
+
+class Bow: Weapon {
+    
+    
+    init() {
+        super.init(name: "arc", damage: 5)
+    }
+    
+    
+    func randomDamage() -> Int{
+        let randomDamage = Int(arc4random_uniform(UInt32(21)))
+        return randomDamage
+    }
+}

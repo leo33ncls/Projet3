ebf020501363f861686a0176b6b40bd1d3111d76
Léo Nicolas <leoncls3398@gmail.com>
2019-01-16 22:03:40 +0100

    Modif du role du Sorcier Wizard()

--- Projet3/Class/Character/Wizard.swift
+++ Projet3/Class/Character/Wizard.swift
@@ -11,6 +11,36 @@ import Foundation
 class Wizard: Character {
     
     init(characterName: String) {
-        super.init(characterName: characterName, type: "Sorcier", health: 60, healthMax: 60, weapon: Curse())
+        super.init(characterName: characterName, type: "Sorcier", health: 60, weapon: Curse())
     }
+    
+    override func attack(target: Character) -> Int {
+        if target is Magus {
+            if  -weapon.damage < target.weapon.damage {
+                target.weapon.damage = 0
+            } else {
+                target.weapon.damage += weapon.damage
+            }
+            
+        } else if target is Wizard {
+            target.weapon.damage = 0
+            weapon.damage = 0
+            
+        } else if let targetWeapon = target.weapon as? RangedWeapon {
+            if targetWeapon.damageMax < weapon.damage {
+                targetWeapon.damageMax = targetWeapon.damageMin
+            } else {
+                targetWeapon.damageMax -= weapon.damage
+            }
+            
+        } else {
+            if target.weapon.damage < weapon.damage {
+                target.weapon.damage = 0
+            } else {
+                target.weapon.damage -= weapon.damage
+            }
+        }
+        return weapon.damage
+    }
+    
 }
